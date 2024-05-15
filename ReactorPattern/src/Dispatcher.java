69eea1c288570c71f186b8327226d55da771110d
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-05-15 18:04:26 +0900

    Feat: 추가. 실제 통신

--- ReactorPattern/src/Dispatcher.java
+++ ReactorPattern/src/Dispatcher.java
@@ -1,9 +1,12 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Dispatcher {
+
 
     public void callHandler(ServerSocket serverSocket, EventHandlerMap handlerMap) throws IOException {
         Socket socket = serverSocket.accept();
@@ -12,11 +15,15 @@ public class Dispatcher {
     }
 
     public String findHandler(Socket socket) throws IOException {
+        System.out.println("Dispatcher.findHandler");
         InputStream inputStream = socket.getInputStream();
 
-        byte[] buffer = new byte[30];
+        byte[] buffer = new byte[1024];
         inputStream.read(buffer);
         String header = new String(buffer);
+        header = header.substring(header.indexOf("Event"), header.indexOf("|"));
+        header = header.substring(7);
+        System.out.println(header);
         return header;
     }
 }
--- ReactorPattern/src/Reactor.java
+++ ReactorPattern/src/Reactor.java
@@ -1,4 +1,5 @@
 import events.EventHandler;
+import events.EventOne;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -7,6 +8,12 @@ public class Reactor {
 
     private ServerSocket serverSocket;
     private EventHandlerMap handlerMap;
+
+    public static void main(String[] args) throws IOException {
+        Reactor reactor  = new Reactor(8080);
+        reactor.registerHandler(new EventOne());
+        reactor.runServer();
+    }
 
     public Reactor(int port) throws IOException {
         this.serverSocket = new ServerSocket(port);
